e6874c128251fc2478fe7a1d5a619a0a083f9469
ASH0DANNY <ash22714497@gmail.com>
2024-05-02 11:59:57 +0530

    Added Data

--- src/components/DueRecieptGenerator/DueRecieptList.ts
+++ src/components/DueRecieptGenerator/DueRecieptList.ts
@@ -1,155 +1,225 @@
 import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 import {
-  EMAIL_ICON,
-  HOME_ICON,
-  LOGO_BASE_64,
-  PHONE_ICON,
-  POPPINS_BOLD,
-  POPPINS_REGULAR,
-  POPPINS_SEMIBOLD,
+    EMAIL_ICON,
+    LOGO_BASE_64,
+    PHONE_ICON,
+    POPPINS_BOLD,
+    POPPINS_REGULAR,
+    POPPINS_SEMIBOLD,
 } from "utilities/Base64Url";
 import {
-  SCHOOL_ADDRESS,
-  SCHOOL_CONTACT,
-  SCHOOL_EMAIL,
-  SCHOOL_NAME,
+    SCHOOL_ADDRESS,
+    SCHOOL_CONTACT,
+    SCHOOL_EMAIL,
+    SCHOOL_NAME,
 } from "config/schoolConfig";
 import { DueRecieptPropsType } from "types/student";
+import { CellInput } from "jspdf-autotable";
+
+let DueRecieptListHeader=[
+    "Sl no.",
+    "Session",
+    "Due Month",
+    "Due Date",
+    "Name",
+    "Ad no",
+    "Class",
+    "Roll",
+    "Contact",
+    "Due Amount",
+    "Remark",
+];
+
+// type ListArr={
+//     sl:String,
+//     session:String,
+//     due_month:String,
+//     due_date:String,
+//     stud_name:String,
+//     stud_adno:String,
+//     stud_class:String,
+//     stud_sec:String,
+//     stud_rol:String,
+//     stud_contact:String,
+//     due_amount:String,
+//     remark:String,
+// };
 
 export const DueRecieptList = async (
-  recieptData: DueRecieptPropsType[]
+    recieptData: DueRecieptPropsType[]
 ): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    try {
-      const doc = new jsPDF({
-        orientation: "p",
-        unit: "mm",
-        format: "a4",
-      });
 
-      const cardWidth = doc.internal.pageSize.getWidth() - 15;
-      const cardHeight = doc.internal.pageSize.getHeight() - 15;
-      const margin = 2;
+    let DueRecieptListArr:DueRecieptPropsType[]=[];
 
-      const x = 5 + margin;
-      const y = 5 + margin;
-      // const y=cardHeight+margin;
+    return new Promise((resolve, reject) => {
+        try {
+            const doc = new jsPDF({
+                orientation: "p",
+                unit: "mm",
+                format: "a4",
+            });
 
-      doc.setTextColor("#000");
+            const cardWidth = doc.internal.pageSize.getWidth() - 15;
+            const cardHeight = doc.internal.pageSize.getHeight() - 15;
+            const margin = 2;
 
-      // Load fonts
-      doc.addFileToVFS("Poppins-Bold", POPPINS_BOLD);
-      doc.addFont("Poppins-Bold", "Poppins", "bold");
+            const x = 5 + margin;
+            const y = 5 + margin;
+            // const y=cardHeight+margin;
 
-      doc.addFileToVFS("Poppins-Regular", POPPINS_REGULAR);
-      doc.addFont("Poppins-Regular", "Poppins", "normal");
+            doc.setTextColor("#000");
 
-      doc.addFileToVFS("Poppins-Semibold", POPPINS_SEMIBOLD);
-      doc.addFont("Poppins-Semibold", "Poppins", "semibold");
-      ///Start of PDF Design
+            // Load fonts
+            doc.addFileToVFS("Poppins-Bold", POPPINS_BOLD);
+            doc.addFont("Poppins-Bold", "Poppins", "bold");
 
-      doc.addImage(LOGO_BASE_64, x + 8, y + 2, 30, 25);
+            doc.addFileToVFS("Poppins-Regular", POPPINS_REGULAR);
+            doc.addFont("Poppins-Regular", "Poppins", "normal");
 
-      const schoolHeaderStartX = x + 50;
-      const schoolHeaderStartY = y + 5;
+            doc.addFileToVFS("Poppins-Semibold", POPPINS_SEMIBOLD);
+            doc.addFont("Poppins-Semibold", "Poppins", "semibold");
+            ///Start of PDF Design
 
-      doc.setFontSize(15);
-      doc.setFont("Poppins", "bold");
-      doc.text(SCHOOL_NAME, schoolHeaderStartX + 10, schoolHeaderStartY);
+            doc.addImage(LOGO_BASE_64, x + 8, y + 2, 30, 25);
 
-      doc.setFontSize(8);
-      doc.setFont("Poppins", "semibold");
-      doc.text(
-        "An English Medium School Based on CBSE Syllabus",
-        schoolHeaderStartX + 7,
-        schoolHeaderStartY + 5
-      );
+            const schoolHeaderStartX = x + 50;
+            const schoolHeaderStartY = y + 5;
 
-      const schoolContactDetailStartY = schoolHeaderStartY + 2;
-      // const schoolContactDetailStartX = schoolHeaderStartX - 5;
+            doc.setFontSize(15);
+            doc.setFont("Poppins", "bold");
+            doc.text(SCHOOL_NAME, schoolHeaderStartX + 10, schoolHeaderStartY);
 
-      const cardXStartPoint = x;
-      const cardXEndPoint = cardWidth;
+            doc.setFontSize(8);
+            doc.setFont("Poppins", "semibold");
+            doc.text(
+                "An English Medium School Based on CBSE Syllabus",
+                schoolHeaderStartX + 7,
+                schoolHeaderStartY + 5
+            );
 
-      doc.setFillColor("#cbc9c9");
-      doc.rect(
-        schoolHeaderStartX + 5,
-        schoolContactDetailStartY + 5,
-        cardXEndPoint - 140,
-        4,
-        "F"
-      );
+            const schoolContactDetailStartY = schoolHeaderStartY + 2;
+            // const schoolContactDetailStartX = schoolHeaderStartX - 5;
 
-      doc.setFontSize(6);
-      doc.setFont("Poppins", "normal");
-      doc.text(
-        SCHOOL_ADDRESS,
-        schoolHeaderStartX + 12,
-        schoolContactDetailStartY + 7.5
-      );
+            const cardXStartPoint = x;
+            const cardXEndPoint = cardWidth;
 
-      //school contact
-      doc.addImage(
-        PHONE_ICON,
-        schoolHeaderStartX + 9,
-        schoolContactDetailStartY + 10,
-        3,
-        3
-      );
+            doc.setFillColor("#cbc9c9");
+            doc.rect(
+                schoolHeaderStartX + 5,
+                schoolContactDetailStartY + 5,
+                cardXEndPoint - 140,
+                4,
+                "F"
+            );
 
-      doc.text(
-        SCHOOL_CONTACT,
-        schoolHeaderStartX + 13,
-        schoolContactDetailStartY + 12
-      );
+            doc.setFontSize(6);
+            doc.setFont("Poppins", "normal");
+            doc.text(
+                SCHOOL_ADDRESS,
+                schoolHeaderStartX + 12,
+                schoolContactDetailStartY + 7.5
+            );
 
-      //school email
-      doc.addImage(
-        EMAIL_ICON,
-        schoolHeaderStartX + 34,
-        schoolContactDetailStartY + 10,
-        3,
-        3
-      );
+            //school contact
+            doc.addImage(
+                PHONE_ICON,
+                schoolHeaderStartX + 9,
+                schoolContactDetailStartY + 10,
+                3,
+                3
+            );
 
-      doc.text(
-        SCHOOL_EMAIL,
-        schoolHeaderStartX + 38,
-        schoolContactDetailStartY + 12
-      );
+            doc.text(
+                SCHOOL_CONTACT,
+                schoolHeaderStartX + 13,
+                schoolContactDetailStartY + 12
+            );
 
-      doc.setFillColor("#939393");
+            //school email
+            doc.addImage(
+                EMAIL_ICON,
+                schoolHeaderStartX + 34,
+                schoolContactDetailStartY + 10,
+                3,
+                3
+            );
 
-      doc.rect(cardXStartPoint, y + 26, cardXEndPoint, 6, "F");
+            doc.text(
+                SCHOOL_EMAIL,
+                schoolHeaderStartX + 38,
+                schoolContactDetailStartY + 12
+            );
 
-      doc.setFont("Poppins", "semibold");
-      doc.setFontSize(9);
-      doc.setTextColor("#fff");
-      doc.text("Daily Attendance Report", cardWidth / 2 - 8, y + 30);
+            doc.setFillColor("#939393");
 
-        
+            doc.rect(cardXStartPoint, y + 26, cardXEndPoint, 6, "F");
 
-        ///End Of PDF DESIGN
-        // Draw border around content
-        doc.setDrawColor("#949494");
-        doc.rect(x, y, cardWidth, cardHeight);
+            doc.setFont("Poppins", "semibold");
+            doc.setFontSize(9);
+            doc.setTextColor("#fff");
+            doc.text("Due Reciept List", cardWidth / 2 - 8, y + 30);
 
-        recieptData.forEach((data, index) => {
+
+            let tableX = x - 3;
+            let tableY = y + 43.5;
+
+            autoTable(doc, {
+                head: [DueRecieptListHeader],
+                body: recieptData.map((item) => {
+                    var tempArr: CellInput[] = [];
+                    const stringArr: string[] = [];
+                    stringArr.push("");
+                    stringArr.push(item.section);
+                    stringArr.push(item.due_month);
+                    stringArr.push(item.due_date);
+                    stringArr.push(item.student_name);
+                    stringArr.push(item.admission_no);
+                    stringArr.push(item.class);
+                    stringArr.push(item.section);
+                    stringArr.push(item.roll_number.toString());
+                    stringArr.push(item.phone_number.toString());
+                    stringArr.push("");
+                    tempArr.push(stringArr);
+                    return tempArr;
+                  }),
+                startY: tableY,
+                theme: "grid",
+                styles: {
+                  textColor: "#000",
+                  fontSize: 8,
+                },
+                margin: { left: tableX + 20 },
+                headStyles: {
+                  cellWidth: 20,
+                  fillColor: "#fff",
+                  textColor: "#000",
+                  minCellHeight: 4,
+                },
+              });
+
+
+            ///End Of PDF DESIGN
             // Draw border around content
             doc.setDrawColor("#949494");
             doc.rect(x, y, cardWidth, cardHeight);
-    
-        if (index === recieptData.length - 1) {
-          // Save PDF and update state with URL
-          const blob = doc.output("blob");
-          // doc.save("save.pdf")
-          var blobUrl = URL.createObjectURL(blob);
 
-          resolve(blobUrl);
+            recieptData.forEach((data, index) => {
+                // Draw border around content
+                doc.setDrawColor("#949494");
+                doc.rect(x, y, cardWidth, cardHeight);
+
+                if (index === recieptData.length - 1) {
+                    // Save PDF and update state with URL
+                    const blob = doc.output("blob");
+                    // doc.save("save.pdf")
+                    var blobUrl = URL.createObjectURL(blob);
+
+                    resolve(blobUrl);
+                }
+            });
+        } catch (error) {
+            reject(error);
         }
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
+    });
 };
