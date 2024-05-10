ac56e84702dfc2cb5e7add73115787bc7e1a662b
ASH0DANNY <ash22714497@gmail.com>
2024-05-10 16:19:45 +0530

    Dummu Data added

--- src/components/StudentDetailsReport/StudentReportGenerator.ts
+++ src/components/StudentDetailsReport/StudentReportGenerator.ts
@@ -2,8 +2,9 @@ import * as XLSX from 'xlsx';
 import { LOGO_BASE_64 } from 'utilities/Base64Url';
 
 const studentData = [  // Replace with your actual student data
-    { Name: "John Doe", ID: 123, Grade: "A" },
-    { Name: "Jane Smith", ID: 456, Grade: "B" },
+    { ID: 123, Name: "John Doe", Fathername: "John Daddy", Class: "Nursary", Section:"A", Roll: "17", Contact: "987563269", Address:"Ratu Ranchi"},
+    { ID: 123, Name: "Harry Potter", Fathername: "Harry Daddy", Class: "Nursary", Section:"B", Roll: "02", Contact: "983455559", Address:"Lalpur Ranchi"},
+    { ID: 456, Name: "Jane Smith", Fathername: "Jane Daddy", Class: "Nursary", Section:"B", Roll: "32", Contact: "987563269", Address:"Kadru Ranchi"},
 ];
 
 const companyName="Company Name";
@@ -16,9 +17,11 @@ const StudDataExcel = () => {
   XLSX.utils.sheet_add_aoa(worksheet,[[{ t: 's', s: { fill: { fgColor: { rgb: 'FFFF00' } } }, h: '<img src="data:image/png;base64,' + LOGO_BASE_64 + '" />' }]]);
     XLSX.utils.book_append_sheet(workbook, worksheet, "Student Data");
 
+    XLSX.utils.sheet_add_aoa(worksheet, [[ "ID","Name","Fathername","Class","Section","Roll","Contact","Address"]], { origin: { c: 0, r: 2 } });
+    
     for (let i = 0; i < studentData.length; i++) {
-      const currentRow = i + 2; 
-      XLSX.utils.sheet_add_aoa(worksheet, [[studentData[i].Name, studentData[i].ID, studentData[i].Grade]], { origin: { c: 0, r: currentRow } });
+      const currentRow = i + 3; 
+      XLSX.utils.sheet_add_aoa(worksheet, [[ studentData[i].ID, studentData[i].Name, studentData[i].Fathername, studentData[i].Class, studentData[i].Section, studentData[i].Roll, studentData[i].Contact, studentData[i].Address]], { origin: { c: 0, r: currentRow } });
     }
 
     
